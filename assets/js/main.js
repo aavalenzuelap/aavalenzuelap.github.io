d9d0bd4bb64b7acf21ca5fb75e6a11217139967b
Alexander Valenzuela <alexandervalenzuelapimentel@gmail.com>
2024-11-16 23:09:28 -0300

    Sidebar closed when initializing

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,8 @@
 			});
 
 			breakpoints.on('>large', function() {
-				$sidebar.removeClass('inactive');
+				// $sidebar.removeClass('inactive');
+				$sidebar.addClass('inactive');
 			});
 
 		// Hack: Workaround for Chrome/Android scrollbar position bug.
